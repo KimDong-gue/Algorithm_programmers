ec19fa59614a737a5be6fc62ded5e8e1a6a30afa
KimDong-gue <116249934+KimDong-gue@users.noreply.github.com>
2023-12-15 09:20:29 +0900

    [level unrated] Title: l로 만들기, Time: 0.66 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.py
@@ -0,0 +1,23 @@
+def solution(myString):
+    
+    a=myString
+    a=a.replace('a','l')
+    a=a.replace('b','l')
+    a=a.replace('c','l')
+    a=a.replace('d','l')
+    a=a.replace('e','l')
+    a=a.replace('g','l')
+    a=a.replace('f','l')
+    a=a.replace('h','l')
+    a=a.replace('i','l')
+    a=a.replace('j','l')
+    a=a.replace('k','l')
+    return a    
+    
+    
+    
+    
+    
+    
+    
+    
