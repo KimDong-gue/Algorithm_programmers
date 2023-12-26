1f03f60e0aac93baf214393f91f02b7bbd499b6f
KimDong-gue <116249934+KimDong-gue@users.noreply.github.com>
2023-12-26 13:31:56 +0900

    [level unrated] Title: 피자 나눠 먹기 (1), Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
+++ 프로그래머스/unrated/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
@@ -1,8 +1,4 @@
 def solution(n):
-    answer = 0
-    if n % 7 ==0:
-        answer = n // 7
-    else:
-        answer = n // 7 + 1
-        
-    return answer    
+    if n // 7 > 0:
+        print(1)
+    
