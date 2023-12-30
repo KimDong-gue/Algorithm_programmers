dd8b3ac5e358b741d9e7b2d2eccce2110574cfcb
KimDong-gue <116249934+KimDong-gue@users.noreply.github.com>
2023-12-30 13:46:43 +0900

    [level unrated] Title: 길이에 따른 연산, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.py
@@ -0,0 +1,13 @@
+def solution(num_list):
+    a=0
+    b=1
+    for i in num_list:
+        if len(num_list) >=11:
+            a+=i
+            pass
+            c=a
+        else:
+            b*=i
+            c=b
+    return c
+    
